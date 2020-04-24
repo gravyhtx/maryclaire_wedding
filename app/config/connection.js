a6dc8d1d7e71be0bbe6f19b250f7d7eafda14fb8
GravyHTX <godisgravy@gmail.com>
2020-04-23 19:12:08 -0500

    Set up for Jaws

--- app/config/connection.js
+++ app/config/connection.js
@@ -4,17 +4,22 @@
 
 // Require mysql
 var mysql = require("mysql");
-
+var connection;
 // Set up our connection information
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "mc_guestbook_db"
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "mc_guestbook_db"
+  });
+}
 
 // Connect to the database
+
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
