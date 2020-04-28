01b279f3533893a0d94965a6da8d2027b07e1747
GravyHTX <godisgravy@gmail.com>
2020-04-28 03:29:35 -0500

    Attempting to fix DB issue

--- schema.sql
+++ schema.sql
@@ -2,11 +2,11 @@
 -- CREATE DATABASE mc_guestbook_db;
 USE g8iknkbvx7z5mfqe;
 
-CREATE TABLE `chirps` (
+CREATE TABLE chirps (
   id INT AUTO_INCREMENT NOT NULL,
   author VARCHAR( 255) NOT NULL,
   body VARCHAR( 255 ) NOT NULL,
   created_at DATETIME NOT NULL,
 
-  PRIMARY KEY ( `id` ) 
+  PRIMARY KEY ( id ) 
 );
