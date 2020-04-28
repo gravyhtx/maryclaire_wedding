bef61ab643856fc655dcfe1b0671deeb869fad74
GravyHTX <godisgravy@gmail.com>
2020-04-28 03:29:08 -0500

    Attempting to fix DB issue

--- schema.sql
+++ schema.sql
@@ -3,10 +3,10 @@
 USE g8iknkbvx7z5mfqe;
 
 CREATE TABLE `chirps` (
-  `id` INT AUTO_INCREMENT NOT NULL,
-  `author` VARCHAR( 255) NOT NULL,
-  `body` VARCHAR( 255 ) NOT NULL,
-  `created_at` DATETIME NOT NULL,
+  id INT AUTO_INCREMENT NOT NULL,
+  author VARCHAR( 255) NOT NULL,
+  body VARCHAR( 255 ) NOT NULL,
+  created_at DATETIME NOT NULL,
 
   PRIMARY KEY ( `id` ) 
 );
