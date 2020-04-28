f441b337e96ef8805d87cfbf7f7f1de7da0fe592
GravyHTX <godisgravy@gmail.com>
2020-04-28 02:25:58 -0500

    Attempting to fix DB issue

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
 -- DROP DATABASE IF EXISTS mc_guestbook_db;
-CREATE DATABASE mc_guestbook_db;
-USE mc_guestbook_db;
+-- CREATE DATABASE mc_guestbook_db;
+USE g8iknkbvx7z5mfqe;
 
 CREATE TABLE `chirps` (
   `id` INT AUTO_INCREMENT NOT NULL,
